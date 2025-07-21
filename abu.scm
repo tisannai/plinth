f9621467f0ee0e88b6532586032f833b8bc98c19
Tero Isannainen <tero.isannainen@gmail.com>
2025-07-21 12:13:08 +0300

    Removed unused files.

--- abu.scm
+++
@@ -1,9 +0,0 @@
-(list
- (cons "plinth"
-       (list (cons 'project-type          "c-library-shared")
-             (cons 'source-directory      "src")
-             (cons 'compile-options       (list "-fPIC" "-g" "-O0"))
-             (cons 'ceedling-options      (list "-fPIC" "-g" "-O0"))
-             (cons 'version               "0.0.1")
-             (cons 'actions               (list "version" "clean" "compile" "link" "publish"))))
- )
